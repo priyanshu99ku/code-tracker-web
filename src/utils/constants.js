1bd32deec90065066186a8df302d43d93c49b8c6
priyanshu99ku <156918107+priyanshu99ku@users.noreply.github.com>
2025-06-18 14:12:30 +0530

    change in base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // API base URL constant for use throughout the app
-export const API_BASE_URL = "http://localhost:3000/api";
+export const API_BASE_URL = "https://code-tracker-backend-0tk4.onrender.com/api";
