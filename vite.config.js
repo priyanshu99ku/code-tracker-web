b09f03f80dad48916cd011c84fe26b525559a9a2
priyanshu99ku <156918107+priyanshu99ku@users.noreply.github.com>
2025-06-18 20:06:12 +0530

    bug in render

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,20 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('recharts')) {
+              return 'vendor_recharts';
+            }
+            if (id.includes('react-icons')) {
+                return 'vendor_react-icons';
+            }
+            return 'vendor'; // all other node_modules
+          }
+        },
+      },
+    },
   },
 })
