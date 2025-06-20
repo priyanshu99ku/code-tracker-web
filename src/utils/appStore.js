aff635d222c7db0abfa038a8ea0f48c2034b10fa
priyanshu99ku <156918107+priyanshu99ku@users.noreply.github.com>
2025-06-20 15:13:40 +0530

    persist

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,12 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './userSlice';
 import profileReducer from './profileSlice';
 
-const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'] // We will only persist the user slice
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     profile: profileReducer,
-  },
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
